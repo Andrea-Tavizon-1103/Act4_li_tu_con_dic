737ed4412324235900f382efd310eb13aa617bdd
Andrea Tavizon <164534025+Andrea-Tavizon-1103@users.noreply.github.com>
2024-09-06 12:14:05 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,6 @@
+numeros=("1", "2", "3", "4")
+print(numeros)
+
+print("-for en conjuntos--")
+for x in numeros:
+    print (x)
---
+++ diccionario.py
@@ -0,0 +1,10 @@
+diccionario = {
+    "dia": 6,
+    "mes": "Septiembre",
+    "año": 2024,
+    "colores": ["rojo", "blaco", "verde"]
+}
+print(diccionario)
+for x in diccionario:
+    print(x)
+    print(diccionario[x])
